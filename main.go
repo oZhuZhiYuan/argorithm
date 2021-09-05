d8ff5cd61a0a67c3d621b4ea54fa968d9e0dc87f
oZhuZhiYuan <izyzhu@163.com>
2021-09-05 23:26:07 +0800

    23:26

--- main.go
+++ main.go
@@ -1,11 +1,4 @@
 package main
 
-import (
-	"argo/problems"
-	"fmt"
-)
-
 func main() {
-	x := problems.MyPow2(2, 5)
-	fmt.Println(x)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -5,7 +5,7 @@ func IntSliceCompare(a, b []int) bool {
 	if len(a) != len(b) {
 		return false
 	}
-	for i, _ := range a {
+	for i := range a {
 		if a[i] != b[i] {
 			return false
 		}
