48b8cf62ebe89b582882447c0dc2c69917820da5
oZhuZhiYuan <izyzhu@163.com>
2021-09-25 17:04:55 +0800

    17:04

---
+++ testcase/200_test.go
@@ -0,0 +1,22 @@
+package testcase
+
+import (
+	"argo/problems"
+	"testing"
+)
+
+func TestNumIslands(t *testing.T) {
+	testcase := [][]byte{
+		{'1', '1', '0', '0', '0'},
+		{'1', '1', '0', '0', '0'},
+		{'0', '0', '1', '0', '0'},
+		{'0', '0', '0', '1', '1'},
+	}
+	expect := 3
+	ret := problems.NumIslands(testcase)
+	if ret != expect {
+		t.Fatalf("test NumIslands fail\nexpect: %d\nresult: %d", expect, ret)
+	}
+
+	t.Logf("test NumIslands success\nexpect: %d\nresult: %d", expect, ret)
+}
