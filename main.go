bcb8feeab290afbf121a6b0b60cdc53a13d2d98c
oZhuZhiYuan <izyzhu@163.com>
2021-10-16 11:56:05 +0800

    11:56

--- main.go
+++ main.go
@@ -1,17 +1,4 @@
 package main
 
-import "fmt"
-
-type Test struct {
-	x int
-}
-
-func (this *Test) add() {
-	this.x++
-}
-
 func main() {
-	t := Test{100}
-	t.add()
-	fmt.Println(t.x)
 }
---
+++ other/numPath.go
@@ -0,0 +1,42 @@
+package other
+
+/*
+动态规划实例问题
+*/
+
+/*
+chess := [][]int{
+	{0, 0, 0, 0, 0, 0, 0, 0},
+	{0, 0, -1, 0, 0, 0, -1, 0},
+	{0, 0, 0, 0, -1, 0, 0, 0},
+	{-1, 0, -1, 0, 0, -1, 0, 0},
+	{0, 0, -1, 0, 0, 0, 0, 0},
+	{0, 0, 0, -1, -1, 0, -1, 0},
+	{0, -1, 0, 0, 0, -1, 0, 0},
+	{0, 0, 0, 0, 0, 0, 0, 0},
+}
+*/
+
+func NumPath(chess [][]int) int {
+	hi := len(chess) - 1
+	wi := len(chess[0]) - 1
+	// 初始化
+	for i := 0; i < hi; i++ {
+		chess[i][wi] = 1
+	}
+	for j := 0; j < wi; j++ {
+		chess[hi][j] = 1
+	}
+	// 状态方程 chess[i][j] = chess[i+1][j] + chess[i][j+1]
+	for i := hi - 1; i >= 0; i-- {
+		for j := wi - 1; j >= 0; j-- {
+			if chess[i][j] == -1 {
+				chess[i][j] = 0
+				continue
+			}
+			chess[i][j] = chess[i+1][j] + chess[i][j+1]
+		}
+	}
+
+	return chess[0][0]
+}
